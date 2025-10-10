a2f38dd2263222cee009508f5ce381630060dcba
Chuabacca <jchua.yf@gmail.com>
2025-10-10 15:28:45 -0700

    optimize bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,12 +1,10 @@
 export default function bubble_sort(arr: number[]): void {
-  for (let j = arr.length - 1; j > 0; j--) {
-    for (let i = 0; i < j; i++) {
-      const left = arr[i]
-      const right = arr[i + 1]
-
-      if (left > right) {
-        arr[i] = right
-        arr[i + 1] = left
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
       }
     }
   }
