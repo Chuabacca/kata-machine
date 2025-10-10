180138a062d3a69aa114698e2b4133623d7408cf
Chuabacca <jchua.yf@gmail.com>
2025-10-10 13:59:12 -0700

    solve two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,16 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+  const interval = Math.floor(Math.sqrt(breaks.length))
+  let i = 0
+  let broken = false
 
+  while (broken === false) {
+    i += interval
+    broken = breaks[i]
+  }
+
+  for (let j = i - interval; j < i; j++) {
+    if (breaks[j] === true) { return j }
+  }
+
+  return -1
 }
