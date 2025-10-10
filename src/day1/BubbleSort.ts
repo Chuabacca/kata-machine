c161b614b987ab8e5c84268302817ef25e4d6c13
Chuabacca <jchua.yf@gmail.com>
2025-10-10 14:59:37 -0700

    solve bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,13 @@
 export default function bubble_sort(arr: number[]): void {
+  for (let j = arr.length - 1; j > 0; j--) {
+    for (let i = 0; i < j; i++) {
+      const left = arr[i]
+      const right = arr[i + 1]
 
+      if (left > right) {
+        arr[i] = right
+        arr[i + 1] = left
+      }
+    }
+  }
 }
